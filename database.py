b7621fb777d3097d27bf3894ef180d4311885c25
Tikava <wavoleniu@gmail.com>
2024-09-22 23:19:45 +0500

    added: db, and more

---
+++ database.py
@@ -0,0 +1,60 @@
+from contextlib import contextmanager
+from sqlalchemy import Column, BigInteger, Integer, String, create_engine, ForeignKey, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    telegram_id = Column(BigInteger, nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+    
+    bots = relationship('Bot', back_populates='user', uselist=False)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, telegram_id={self.telegram_id})>"
+    
+class Bot(Base):
+    __tablename__ = 'bots'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    token = Column(String)
+    user_id = Column(BigInteger, ForeignKey('users.id'), nullable=False)
+    chat_id = Column(BigInteger, ForeignKey('chats.id'), unique=True)
+
+    user = relationship('User', back_populates='bots')
+    chat = relationship('Chat', back_populates='bots')
+
+    def __repr__(self):
+        return f"<Bot(id={self.id}, token={self.token})>"
+
+class Chat(Base):
+    __tablename__ = 'chats'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    external_chat_id = Column(BigInteger)
+
+    bots = relationship('Bot', back_populates='chat')
+
+    def __repr__(self):
+        return f"<Chat(id={self.id}, external_chat_id={self.external_chat_id})>"
+
+
+engine = create_engine('sqlite:///storage.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session  
+        session.commit() 
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
---
+++ main.py
@@ -0,0 +1,103 @@
+from aiogram import Bot, Dispatcher, Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.filters.callback_data import CallbackData
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+
+import aiohttp
+
+from dotenv import load_dotenv
+
+from database import session_scope, User as UserModel, Bot as BotModel, Chat as ChatModel
+
+
+import asyncio
+from os import getenv
+
+load_dotenv()
+
+dp = Dispatcher()
+BOT_TOKEN = getenv("BOT_TOKEN")
+bot = Bot(token=BOT_TOKEN)
+
+bot_form_router = Router()
+
+class CustomCallback(CallbackData, prefix='id'):
+    bot_id: int
+
+class SUPPORT_BOT(StatesGroup):
+    token = State()
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response.json()
+        
+async def fetch_all(urls):
+    tasks = [fetch(url) for url in urls]
+    results = await asyncio.gather(*tasks)
+    return results
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message, state: FSMContext) -> None:
+    with session_scope() as session:
+        user = session.query(UserModel).filter(UserModel.telegram_id == message.from_user.id).first()
+
+    if user:
+        await message.answer('Вы уже вы зарегистрированы.')
+    else:
+        with session_scope() as session:
+            new_user = UserModel(telegram_id=message.from_user.id)
+            session.add(new_user)
+
+        await state.set_state(SUPPORT_BOT.token)
+        await message.answer("Чтобы создать бота мне нужен токен, пожалуйста отправьте мне его. Токен можно получить у @BotFather.")
+
+@bot_form_router.message(SUPPORT_BOT.token)
+async def token_input_handler(message: Message, state: FSMContext) -> None:
+    user_answer = message.text
+    response = await fetch(f"https://api.telegram.org/bot{user_answer}/getMe")
+    if response['ok']:
+
+        with session_scope() as session:
+            new_bot = BotModel(token=user_answer, user_id=message.from_user.id)
+            session.add(new_bot)
+
+        await state.clear()
+        await message.answer('Ваш бот успешно привязан!')
+    else:
+        await state.set_state(SUPPORT_BOT.token)
+        await message.answer('Не верный токен! Попробуйте снова.')
+
+@dp.message(Command('add_bot'))
+async def add_bot_handler(message: Message, state: FSMContext) -> None:
+    await state.set_state(SUPPORT_BOT.token)
+    await message.answer('Пожалуйста, введите токен бота от @BotFather.')
+
+@dp.message(Command('my_bots'))
+async def view_bots_handler(message: Message, state: FSMContext) -> None:
+    with session_scope() as session:
+        bots = session.query(BotModel).filter(BotModel.user_id == message.from_user.id).all()
+    
+        if bots:
+            urls = [f"https://api.telegram.org/bot{bot.token}/getMe" for bot in bots]
+            responses = await fetch_all(urls)
+            inline_keyboard = [[InlineKeyboardButton(text=response['result']['first_name'], callback_data=CustomCallback(bot_id=response['result']['id']).pack())] for response in responses]
+            markup = InlineKeyboardMarkup(inline_keyboard=inline_keyboard)
+
+            await message.answer('Список ботов:', reply_markup=markup)
+        else:
+            await message.answer('У вас нет привязанных ботов.')
+
+@dp.callback_query(CustomCallback.filter(F.bot_id))
+async def handle_telegram_id(query: CallbackQuery, callback_data: CustomCallback) -> None:
+    await query.message.edit_text('К этому боту не привязана группа.')
+
+async def main() -> None:
+    dp.include_router(bot_form_router)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
